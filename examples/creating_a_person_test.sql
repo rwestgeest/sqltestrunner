aff57230a869c417f015a891edd1304695c190db
Rob Westgeest <rob@qwan.it>
2013-03-28 08:34:17 +0100

    updated example to remote curly brackets

--- examples/creating_a_person_test.sql
+++ examples/creating_a_person_test.sql
@@ -1,25 +1,25 @@
 before "creating the person table" do
-  execute %Q{
+  execute """
     CREATE TABLE IF NOT EXISTS Person(
       Id INTEGER PRIMARY KEY,
       Name VARCHAR(10),
       Address VARCHAR(30));
-  }
+  """
 end
 
-test_case "create a person" do
+test_case "1 create a person" do
   stand 0 do
-    execute %Q{
+    execute """
       INSERT INTO Person(Name, Address) VALUES('Gijs', 'Heerlen');
-    }
+    """
     @person_id = last_insert_id
   end
 
   stand 1 do 
-    execute( %Q{
-      SELECT * from Person WHERE Id = #{@person_id}
-    }).should == [
-      [1, 'Gijs', 'Heerlen']
+    execute("""
+      SELECT Name, Address from Person WHERE Id = #{@person_id}
+    """).should == [
+      ['Gijs', 'Heerlen']
     ]
 
   end
@@ -27,7 +27,7 @@ end
 
 test_case "silly named person" do
   stand 0 do 
-    execute %Q{INSERT INTO Person(Name, Address) VALUES('$%$#%$#', 'Heerlen');}
-    execute(%Q{SELECT Name FROM Person WHERE Id = '#{last_insert_id}';}).should == [ ['$%$#%$#'] ]
+    execute "INSERT INTO Person(Name, Address) VALUES('$%$#%$#', 'Heerlen');"
+    execute("SELECT Name FROM Person WHERE Id = '#{last_insert_id}';i").should == [ ['$%$#%$#'] ]
   end
 end
