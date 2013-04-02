1124cb3d1553127b5edd31f20344061f3dfcb437
Rob Westgeest <rob@qwan.it>
2013-04-02 14:58:59 +0200

    brackets fir clarity'

--- lib/sql_test_runner.rb
+++ lib/sql_test_runner.rb
@@ -18,7 +18,7 @@ module SqlTestRunner
     end
 
     def run(test_description)
-      load_tests test_description
+      load_tests(test_description)
       run_stands
       step_logger.log_results
     end
